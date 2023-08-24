5261477e1d05bf0f14443295984d88352e6bbf21
romeoRel <romeorel99@gmail.com>
2023-08-24 19:13:40 +0200

    Adding images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import ProfileCard from './ProfileCard'
+import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+
+console.log(AlexaImage);
+console.log(SiriImage);
 
 function App() {
     return (
         <div>
-        <div>Personal Assistant</div>
+            <div>Personal Assistant</div>
 
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
+            <ProfileCard title = "Alexa" handle= "@alexa99" />
+            <ProfileCard title = "Cortana" handle= "@cortana32" />
+            <ProfileCard title = "Siri" handle= "@siri01" />
         </div>
     );
 
